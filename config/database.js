a29cde1b007b631bb450a9af6f6a6d0893f40cbe
aniketkrraj746 <aniketkrraj746@gmail.com>
2024-02-25 04:04:38 +0530

    variables added

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 require("dotenv").config;
 const faker = require("faker");
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "backlight",
-  port:"3306"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.port,
 });
 db.connect((err) => {
   if (err) {
