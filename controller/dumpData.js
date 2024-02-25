0931773dd957193ba64761d1afb137a652702fb0
aniketkrraj746 <aniketkrraj746@gmail.com>
2024-02-26 00:42:11 +0530

    table manipulated

--- controller/dumpData.js
+++ controller/dumpData.js
@@ -1,13 +1,16 @@
 const faker = require("faker");
-const db = require("../config/database")
-const dumpData=()=>{
+const db = require("../config/database");
+const dumpData = () => {
   for (let i = 0; i < 10000; i++) {
     const UID = i;
-    const Name = `user${i}` || i ; // Generate a fake name
+    const Name = `user${i}` || i; // Generate a fake name
     const Score = Math.floor(Math.random() * 1000) + 1; // Random score between 1 and 1000
     const Country = +Math.floor(Math.random() * 100) + 1; // generate country code between 1 t0 100
     const Timestamp = faker.date
-      .between(new Date(Date.now()), new Date(Date.now() - 7 * 24 * 60 * 60 * 1000))
+      .between(
+        new Date(Date.now() + 20 * 24 * 60 * 60 * 1000),
+        new Date(Date.now() - 20 * 24 * 60 * 60 * 1000)
+      )
       .toISOString()
       .slice(0, 19)
       .replace("T", " "); // Random timestamp between 2022-01-01 and 2024-01-01
